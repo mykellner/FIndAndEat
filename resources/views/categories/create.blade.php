600ae0372adbebcbd60c1e0f2abe6681d9b5b2e5
Elle <eleonormcdowell@gmail.com>
2021-03-29 15:07:22 +0200

    added layout

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,22 +1,23 @@
 @extends('layouts/app')
 
 @section('content')
- <h1 class="mb-3">Create a new Category</h1>
+<div class="container py-4">
+	<h1 class="mb-3">Create a new Category</h1>
 
- <form class="form" action='{{ route('categories.store', ['county' => $county, 'city' => $city])}} ' method="POST">
- @csrf
+	<form class="form" action='{{ route('categories.store', ['county' => $county, 'city' => $city])}} ' method="POST">
+	@csrf
 
- <div class="mb-3">
-    <label for="title" class="form-label">Name of Category</label>
-    <input type="text" id="name" name="name" class="form-control" placeholder="Name" required>
- </div>
+	<div class="mb-3">
+		<label for="title" class="form-label">Name of Category</label>
+		<input type="text" id="name" name="name" class="form-control" placeholder="Name" required>
+	</div>
 
- <button type="submit" class="btn btn-success w-100">Create</button>
- </form>
+	<button type="submit" class="btn btn-success w-100">Create</button>
+	</form>
 
+	<div class="mt-4">
+		<a href="{{ route('cities.show', ['county' => $county, 'city' => $city]) }}" class="btn btn-secondary">&laquo; Back</a>
+	</div>
 
- <div class="mt-4">
-    <a href="{{ route('cities.show', ['county' => $county, 'city' => $city]) }}" class="btn btn-secondary">&laquo; Back</a>
- </div>
-
+</div>
 @endsection
